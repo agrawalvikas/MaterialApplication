b9f9230feaf02093b559c1e940c90b96100f8fca
Vikas Agrawal <vikas.agrawal375@gmail.com>
2015-06-27 03:09:21 +0530

    Added ActionBar Icons

--- app/src/main/java/com/exapmle/materialapplication/MainActivity.java
+++ app/src/main/java/com/exapmle/materialapplication/MainActivity.java
@@ -2,14 +2,17 @@ package com.exapmle.materialapplication;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.support.v7.app.ActionBar;
 import android.widget.Button;
+import android.widget.Toast;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends ActionBarActivity {
     Button go;
 
     @Override
@@ -30,7 +33,7 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
@@ -42,6 +45,11 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            Toast.makeText(getApplicationContext(),"Settings",Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        else if (id== R.id.action_search){
+            Toast.makeText(getApplicationContext(),"Search",Toast.LENGTH_SHORT).show();
             return true;
         }
 
